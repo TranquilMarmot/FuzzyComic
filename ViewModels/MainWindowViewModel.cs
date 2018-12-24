530b15e58ca855dca9bec673b4b72cd7b976301f
TranquilMarmot <nate272@gmail.com>
2018-12-24 01:11:55 -0800

    Choose and start to extract file

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reactive;
 using System.Text;
+using Avalonia.Controls;
+using Avalonia.Media.Imaging;
 using ReactiveUI;
+using SharpCompress.Readers;
 
 namespace FuzzyComic.ViewModels
 {
@@ -16,10 +20,38 @@ namespace FuzzyComic.ViewModels
 
         public ReactiveCommand<Unit, Unit> DoTheThing { get; }
 
+        public Bitmap CurrentImage { get; private set; }
 
-        void RunTheThing()
+
+        async void RunTheThing()
         {
-            System.Console.WriteLine("Open a file!");
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Pick a comic";
+            dialog.AllowMultiple = false;
+            dialog.Filters.Add(new FileDialogFilter() { Name = "All", Extensions = { "*" } });
+            dialog.Filters.Add(new FileDialogFilter() { Name = "Comic Book ZIP", Extensions = { "cbz" } });
+            dialog.Filters.Add(new FileDialogFilter() { Name = "Comic Book RAR", Extensions = { "cbr" } });
+
+            var result = await dialog.ShowAsync();
+            if (result != null)
+            {
+                var chosenPath = result[0];
+
+                using (var fileStream = File.OpenRead(chosenPath))
+                using (var reader = ReaderFactory.Open(fileStream))
+                {
+                    while (reader.MoveToNextEntry())
+                    {
+                        if (!reader.Entry.IsDirectory)
+                        {
+                            var entryStream = reader.OpenEntryStream();
+                            var bitmap = new Bitmap(entryStream);
+                            CurrentImage = bitmap;
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
