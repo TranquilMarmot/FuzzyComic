69c9652b7b82579d65d513bb431e720d81e6d1a1
TranquilMarmot <nate272@gmail.com>
2018-12-24 00:36:14 -0800

    Project start!

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+using ReactiveUI;
+
+namespace FuzzyComic.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            DoTheThing = ReactiveCommand.Create(RunTheThing);
+        }
+        public string Greeting => "Hello World!";
+
+        public ReactiveCommand<Unit, Unit> DoTheThing { get; }
+
+
+        void RunTheThing()
+        {
+            System.Console.WriteLine("Open a file!");
+        }
+    }
+}
