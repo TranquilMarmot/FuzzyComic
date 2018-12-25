394ffa7220b68a8e00b92aa61a1ae334f6edc1a1
TranquilMarmot <nate272@gmail.com>
2018-12-25 01:13:11 -0800

    Closer to loading an image... maybe?

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reactive;
 using System.Text;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Media.Imaging;
 using ReactiveUI;
 using SharpCompress.Readers;
+using SkiaSharp;
 
 namespace FuzzyComic.ViewModels
 {
@@ -14,17 +16,20 @@ namespace FuzzyComic.ViewModels
     {
         public MainWindowViewModel()
         {
-            DoTheThing = ReactiveCommand.Create(RunTheThing);
+            DoTheThing = ReactiveCommand.CreateFromTask(RunTheThing);
+            // DoTheThing = ReactiveCommand.Create(RunTheThing);
+
+            Greeting = "Hello";
         }
-        public string Greeting => "Hello World!";
+        public string Greeting { get; private set; }
 
         public ReactiveCommand<Unit, Unit> DoTheThing { get; }
 
         public Bitmap CurrentImage { get; private set; }
 
-
-        async void RunTheThing()
+        async Task RunTheThing()
         {
+            Greeting = "Load that shit";
             var dialog = new OpenFileDialog();
             dialog.Title = "Pick a comic";
             dialog.AllowMultiple = false;
@@ -35,6 +40,7 @@ namespace FuzzyComic.ViewModels
             var result = await dialog.ShowAsync();
             if (result != null)
             {
+                System.Console.WriteLine("hello ello");
                 var chosenPath = result[0];
 
                 using (var fileStream = File.OpenRead(chosenPath))
@@ -45,7 +51,16 @@ namespace FuzzyComic.ViewModels
                         if (!reader.Entry.IsDirectory)
                         {
                             var entryStream = reader.OpenEntryStream();
-                            var bitmap = new Bitmap(entryStream);
+
+                            // TODO this returns null on error
+                            var skiaBitmap = SKBitmap.Decode(entryStream);
+                            var skiaImage = SKImage.FromBitmap(skiaBitmap);
+                            var encoded = skiaImage.Encode();
+
+                            var bitmap = new Bitmap(encoded.AsStream());
+
+                            System.Console.WriteLine(reader.Entry.Key);
+                            Greeting = reader.Entry.Key;
                             CurrentImage = bitmap;
                             break;
                         }
